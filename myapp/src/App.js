5eb3b273d53f8dcace4028723d9a08cb8e1b3444
Bingusala <139640476+Bingusala@users.noreply.github.com>
2024-11-26 09:34:14 +0000

    update App.js to run the project

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,30 +1,39 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route,Link,NavLink} from react-router-dom;
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Contactus from './componets/ContactUs';
+import AboutUs from './componets/AboutUs';
+import Home from './componets/Home';
+import Login from './componets/Login';
+import Dashboard from './componets/Dashboard';
 
 function App() {
-  <return>
+  return(
     <div className="App">
       <Router>
         <nav>
-          <ul>
+           <ul>
+
             <li><Link to="/home">Home</Link></li>
-            <li>  <Link to="/about">About</Link></li>
-             <li> <Link to="/contact">Contact</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/login">Login</Link></li>
            
-          </ul>
+          </ul> 
         </nav>
         <Routes>
           
-            <Route path='/home' element={<Home/>}/>
+             <Route path='/home' element={<Home/>}/>
             <Route path='/about' element={<AboutUs/>}/>
-            <Route path='/contact' element={<Contactus/>}/>
+            <Route path='/contact' element={<Contactus/>}/> 
+            <Route path='/' element={<Login/>}/>
+            <Route path='/dash' element={<Dashboard/>}/>
          
         </Routes>
       </Router>
 
 
     </div>
-  </return>
+  );
 }
 
 export default App;
