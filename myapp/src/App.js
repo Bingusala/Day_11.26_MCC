1d85be87acd48ee7337497cb295e2d2df423501b
Bingusala <139640476+Bingusala@users.noreply.github.com>
2024-11-26 08:46:05 +0000

    Update App.js that add links to each components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from react-router-dom;
 
 function App() {
-  return (
+  <return>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li>  <Link to="/about">About</Link></li>
+             <li> <Link to="/contact">Contact</Link></li>
+           
+          </ul>
+        </nav>
+        <Routes>
+          
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/about' element={<AboutUs/>}/>
+            <Route path='/contact' element={<Contactus/>}/>
+         
+        </Routes>
+      </Router>
+
+
     </div>
-  );
+  </return>
 }
 
 export default App;
